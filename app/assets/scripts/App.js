0278a0855647422b3666af853cfd3d86f74c5c75
codeByScott <scott@codebyscott.io>
2017-01-26 21:04:25 +0900

    complete reveal-on-scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-
-var $ = require('jquery');
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
